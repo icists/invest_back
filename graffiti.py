a8b3c5271189a30b1ea83bb2a520ed721c6b8321
JinwooJeon1024 <104386015+JinwooJeon1024@users.noreply.github.com>
2023-01-03 17:07:04 +0900

    1.3.3

--- graffiti.py
+++ graffiti.py
@@ -24,87 +24,124 @@ firebase_admin.initialize_app(cred,{
 })
 
 global team_number
-team_number = 18
-
-@bot.event
-async def on_ready():
-    print('Bot initialized')
-    print(f'{bot.user} has connected to Discord!')
-    return
+global startup_number
+global startup_list
+team_number = 10
+startup_number = 4
 
 @bot.command(aliases=['hi'])
 async def hello(ctx):
-    await ctx.send(f'Hello, {ctx.author.mention}!')
+    await ctx.send('ver 1.3.0')
 
 @bot.command()
 async def set_round(ctx, set_round_num):
-    if int(set_round_num) == 1 or int(set_round_num) == 2 or int(set_round_num) == 3: #라운드 범위 체크 필요
+    if int(set_round_num) == 0 or int(set_round_num) == 1 or int(set_round_num) == 2 or int(set_round_num) == 3: #라운드 범위 체크 필요
         dir = db.reference('status')
         dir.update({'currentRound': int(set_round_num)})
         await ctx.send(f'ICISTS 투자게임 - 현재 {set_round_num}라운드로 설정되었습니다.')
-    elif int(set_round_num) == 0:
+    elif int(set_round_num) == 999:
         dir = db.reference('status')
         dir.update({'currentRound': int(set_round_num)})
-        await ctx.send(f'ICISTS 투자게임 - 현재 {set_round_num}라운드로 설정되었습니다.\nICISTS 투자게임 - 라운드가 진행되고 있지 않습니다.')
+        await ctx.send(f'ICISTS 투자게임 - 현재 999 라운드로 설정되었습니다.\nICISTS 투자게임 - 현재 설정 중입니다')
     else:
         await ctx.send(f'ICISTS 투자게임 - 올바른 숫자를 입력해주세요.')
 
 @bot.command()
-async def calculate_return(ctx):
+async def function1(ctx):
     dir_round_num = db.reference('status/currentRound')
     round_num = dir_round_num.get() 
     total_investment = 0
     alpha = 1.5
     
-    for startup_num in range(1,9):  
+    for startup_name in startup_list:  
         for team_num in range(1,team_number + 1) :
-            dir_investmentData = db.reference(f'rounds/{round_num}/investAmount/{startup_num}/{team_num}')
+            dir_investmentData = db.reference(f'rounds/{round_num}/investAmount/{startup_name}/{team_num}')
             invest = dir_investmentData.get()
             total_investment += invest
     avg_investment = total_investment / 8
     await ctx.send(f'전체 스타트업 평균 투자액 : {avg_investment}')
 
-    invest_list = [0] * 9 #각 기업별 총 투자액 리스트, 인덱스 0 은 사용하지 않음
-    for startup_num in range(1,9): # 각 기업별 각 팀에게 돌려줄 금액 계산
+    invest_list = [0] * 8 #각 기업별 총 투자액 리스트
+    index = 0
+    for startup_name in startup_list: # 각 기업별 각 팀에게 돌려줄 금액 계산
         total_invest_eachCompany = 0
         for team_num in range(1, team_number + 1) :
-            dir_investmentData = db.reference(f'rounds/{round_num}/investAmount/{startup_num}/{team_num}')
+            dir_investmentData = db.reference(f'rounds/{round_num}/investAmount/{startup_name}/{team_num}')
             invest = dir_investmentData.get()
             total_invest_eachCompany += invest
-        invest_list[startup_num] = total_invest_eachCompany
+        invest_list[index] = total_invest_eachCompany
+        index += 1
     await ctx.send('각 스타트업별 받은 총 투자액 정산이 완료되었습니다.')
     
-    total_valuation = 0
-    for startup_num in range(1,9): # 각 기업별 VC에게 받은 평가
-        dir_total_valuation = db.reference(f'rounds/{round_num}/valuation/{startup_num}')
-        total_valuation += dir_total_valuation.get()
-    avg_valuation = total_valuation / 8
-    await ctx.send(f'각 스타트업에 대한 VC 평가 완료, 평균 점수 : {avg_valuation}')
+    total_score = 0
+    for startup_name in startup_list: # 각 기업별 VC에게 받은 평가
+        dir_total_score = db.reference(f'rounds/{round_num}/score/{startup_name}')
+        total_score += dir_total_score.get()
+    avg_score = total_score / 8
+    await ctx.send(f'각 스타트업에 대한 VC 평가 완료, 평균 점수 : {avg_score}')
 
-    for startup_num in range(1,9):
+    index = 0
+    for startup_name in startup_list:
         for team_num in range(1,team_number+1):
-            dir = db.reference(f'rounds/{round_num}/investResult/{startup_num}/{team_num}')
-            dir_investmentData = db.reference(f'rounds/{round_num}/investAmount/{startup_num}/{team_num}')
-            dir_valuation = db.reference(f'rounds/{round_num}/valuation/{startup_num}')
-            valuation = dir_valuation.get()
+            dir = db.reference(f'rounds/{round_num}/investResult/{startup_name}/{team_num}')
+            dir_investmentData = db.reference(f'rounds/{round_num}/investAmount/{startup_name}/{team_num}')
+            dir_valuation = db.reference(f'rounds/{round_num}/valuation/{startup_name}')
+            dir_score = db.reference(f'rounds/{round_num}/score/{startup_name}')
+            score = dir_score.get()
             invest = dir_investmentData.get()
-            formula = int(((invest_list[startup_num]/avg_valuation)**(alpha - 1)) * invest * (valuation / avg_valuation))
+            valuation = int((invest_list[index]/avg_score)**(alpha) * (score / avg_score) * avg_investment)
+            dir_valuation({valuation})
+            formula = int(((invest_list[index]/avg_score)**(alpha - 1)) * invest * (score / avg_score))
             dir.update({formula}) # update의 새로운 사용
+        index += 1
     await ctx.send(f'ICISTS 투자게임 - {round_num}라운드 각 팀에게 돌려줄 금액 계산이 완료되었습니다. ')
 
 @bot.command()
-async def sum_account(ctx):
+async def function2(ctx):
     dir_round_num = db.reference('status/currentRound')
     round_num = dir_round_num.get() 
 
     for team_num in range(1,team_number+1):
         team_account = 0
-        for startup_num in range(1,9):
-            dir_investResult = db.reference(f'rounds/{round_num}/investResult/{startup_num}/{team_num}')
+        for startup_name in startup_list:
+            dir_investResult = db.reference(f'rounds/{round_num}/investResult/{startup_name}/{team_num}')
             team_account += dir_investResult.get()
         dir_teamAccount = db.reference(f'teams/{team_num}')
         dir_teamAccount.update({'account' : team_account})
 
     await ctx.send(f'ICISTS 투자게임 - {round_num}라운드 각 팀의 투자 결과 정산이 완료되었습니다')
+
+@bot.command()
+async def setting(ctx, round_num):
+    await ctx.send(f'ICISTS 투자게임 - {round_num} 라운드 Firebase 데이터베이스 설정을 시작합니다.\n')
+
+    for team_num in range(1, team_number + 1):
+        dir_score = db.reference(f'rounds/{round_num}/score')
+        dir_score.update({
+            f'{team_num}' : 5
+        })
+    
+    for team_num in range(1, team_number + 1):
+        for startup_name in startup_list:
+            dir_investAmount= db.reference(f'rounds/{round_num}/investAmount')
+            dir_investAmount.update({
+                f'{team_num}' : {
+                    f'{startup_name}' : 0
+                }
+            })
+    for team_num in range(1, team_number + 1):
+        for startup_name in startup_list:
+            dir_investResult= db.reference(f'rounds/{round_num}/investResult')
+            dir_investResult.update({
+                f'{team_num}' : {
+                    f'{startup_name}' : 0
+                }
+            })
+    for startup_name in startup_list :
+        dir_valuation = db.reference(f'rounds/{round_num}/valuation')
+        dir_valuation.update({
+            f'{startup_name}' : 0
+        })
+    await ctx.send(f'ICISTS 투자게임 - Firebase 데이터 기본 설정이 완료되었습니다.\n')
 
 bot.run(os.environ['token'])
