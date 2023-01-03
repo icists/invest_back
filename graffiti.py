49d77c5b2db812ecb36ad388d3a5205b23dcbd64
JinwooJeon1024 <104386015+JinwooJeon1024@users.noreply.github.com>
2023-01-03 20:32:56 +0900

    1.5.3

--- graffiti.py
+++ graffiti.py
@@ -30,7 +30,7 @@ team_number = 18
 
 @bot.command(aliases=['hi'])
 async def hello(ctx):
-    await ctx.send('ver 1.5.2')
+    await ctx.send('ver 1.5.3')
 
 @bot.command()
 async def set_round(ctx, set_round_num):
@@ -114,19 +114,6 @@ async def function2(ctx):
 async def setting(ctx, round_num):
     await ctx.send(f'ICISTS 투자게임 - {round_num} 라운드 Firebase 데이터베이스 설정을 시작합니다.\n')
 
-    for team_num in range(1,team_number+1):
-        dir= db.reference(f'rounds/{round_num}/investAmount')
-        dir.update({
-            f'{team_num}' : 0
-        })
-    
-    for team_num in range(1,team_number+1):
-        dir= db.reference(f'rounds/{round_num}/investResult')
-        dir.update({
-            f'{team_num}' : 0
-        })
-
-    '''
     for team_num in range(1, team_number + 1):
         dir_investAmount= db.reference(f'rounds/{round_num}/investAmount/{team_num}')
         dir_investAmount.update({ 
@@ -151,7 +138,6 @@ async def setting(ctx, round_num):
             f'{startup_list[6]}' : 0,
             f'{startup_list[7]}' : 0
         })
-    '''
 
     for team_num in range(1, team_number + 1):
         dir_score = db.reference(f'rounds/{round_num}/score')
