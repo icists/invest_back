dc49f274623357c4b8ba8d6b4764ec092d38c03b
JinwooJeon1024 <104386015+JinwooJeon1024@users.noreply.github.com>
2023-01-05 01:34:09 +0900

    2.2.1

--- graffiti.py
+++ graffiti.py
@@ -30,7 +30,7 @@ team_number = 24
 
 @bot.command(aliases=['hi'])
 async def hello(ctx):
-    await ctx.send('ver 2.2.0')
+    await ctx.send('ver 2.2.1')
 
 @bot.command()
 async def set_round(ctx, set_round_num):
@@ -69,17 +69,15 @@ async def function1(ctx , input):
     for startup_name in startup_list: # 각 기업별 각 팀에게 돌려줄 금액 계산
         total_invest_eachCompany = 0
         for team_num in range(1, team_number + 1) :
-            dir_investmentData = db.reference(f'rounds/{round_num}/investAmount/{team_num}/{startup_name}')
-            invest = int(dir_investmentData.get())
-            total_invest_eachCompany += invest
+            total_invest_eachCompany += dict_invest[team_num][startup_name]
         invest_list[index] = total_invest_eachCompany
         index += 1
     await ctx.send('각 스타트업별 받은 총 투자액 정산이 완료되었습니다.')
     
     total_score = 0
+    dict_score = db.reference(f'rounds/{round_num}/score')
     for startup_name in startup_list: # 각 기업별 VC에게 받은 평가
-        dir_total_score = db.reference(f'rounds/{round_num}/score/{startup_name}')
-        total_score += dir_total_score.get()
+        total_score += dict_score[startup_name]
     avg_score = total_score / 8
     await ctx.send(f'각 스타트업에 대한 VC 평가 완료, 평균 점수 : {avg_score}')
 
