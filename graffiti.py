111796a786a7ef62178effd3842cae66f3783715
JinwooJeon1024 <104386015+JinwooJeon1024@users.noreply.github.com>
2023-01-04 23:59:12 +0900

    2.1.7

--- graffiti.py
+++ graffiti.py
@@ -30,7 +30,7 @@ team_number = 24
 
 @bot.command(aliases=['hi'])
 async def hello(ctx):
-    await ctx.send('ver 2.1.6')
+    await ctx.send('ver 2.1.7')
 
 @bot.command()
 async def set_round(ctx, set_round_num):
@@ -159,7 +159,7 @@ async def unable(ctx):
 async def setting(ctx, round_num):
     await ctx.send(f'ICISTS 투자게임 - {round_num} 라운드 Firebase 데이터베이스 설정을 시작합니다.\n')
 
-    defaultmoney = 10000000
+    defaultmoney = 1000000
 
     for team_num in range(1,team_number+1):
         dir = db.reference(f'teams/{team_num}')
