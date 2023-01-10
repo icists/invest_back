67816c24238272959df8da6e22b80b55958bf5c2
JinwooJeon1024 <104386015+JinwooJeon1024@users.noreply.github.com>
2023-01-10 10:16:08 +0900

    6팀

--- graffiti.py
+++ graffiti.py
@@ -26,7 +26,7 @@ firebase_admin.initialize_app(cred,{
 global team_number
 global startup_list 
 startup_list = ['QTC','AET','INB','SHZ','RFY','SWT','NUT','NUV']
-team_number = 24
+team_number = 6
 
 @bot.command(aliases=['hi'])
 async def hello(ctx):
